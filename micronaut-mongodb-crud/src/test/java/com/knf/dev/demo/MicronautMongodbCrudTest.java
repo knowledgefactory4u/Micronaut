16f7eed9ddfb62998ab3f91708b7f94dee9dd33c
knowledgefactory4u <54763130+knowledgefactory4u@users.noreply.github.com>
2021-10-25 18:58:31 +0530

    Add files via upload
    init

---
+++ micronaut-mongodb-crud/src/test/java/com/knf/dev/demo/MicronautMongodbCrudTest.java
@@ -0,0 +1,21 @@
+package com.knf.dev.demo;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class MicronautMongodbCrudTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
